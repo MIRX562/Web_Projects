50d23d4636b73e063a867c9574aaabb046bdd710
MIRX562 <88673222+MIRX562@users.noreply.github.com>
2022-08-31 23:09:05 +0700

    up

---
+++ Darkmode_Tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {},
+    screen: {},
+    extend: {
+      colors: {},
+
+      spacing: {},
+    },
+  },
+  plugins: [],
+};
